fbcc9b776f487ffa844f50f7bc0c67308526d510
Abhishek Chopra <abhishekchopra13@hotmail.com>
2021-09-01 00:00:16 +0530

    Changed eight_puzzles.py

--- lab1/eight_puzzle.py
+++ lab1/eight_puzzle.py
@@ -1,4 +1,5 @@
 import sys
+import utils
 
 def readInput(input_path):
 	with open(input_path) as f:
@@ -13,5 +14,12 @@ if __name__ == '__main__':
 		print('Run: eight_puzzle.py <path_to_initial_state> <path_to_final_state>')
 		exit(0)
 	
+	# Ask for Hueristic type
+	print('Enter Hueristic type:')
+	print('1. h1(n) = number of tiles displaced from their destined position.')
+	print('2. h2(n) = sum of Manhattan distance of each tile from the goal')
+	print('3. h(n) = g(n) Best First Search')
+	hueristic_type = int(input())
+
 	start_state = readInput(sys.argv[1])
 	final_state = readInput(sys.argv[2])
