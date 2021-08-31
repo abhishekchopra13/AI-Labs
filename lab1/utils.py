97f4fc079b6e4e44a519908ab9d15ef6a4720420
fliptrail <amishmittal@outlook.com>
2021-09-01 01:04:22 +0530

    use deepcopy

--- lab1/utils.py
+++ lab1/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 
 BLANK_KEY = -1
 ROW_SIZE = 3
@@ -21,41 +22,47 @@ class Matrix:
             if self.blank is None:
                 raise ValueError(f"No blank found in given matrix {mat}")
 
-    def up(self):
-        if self.blank[0] == ROW_SIZE - 1:
-            print("No tile can be moved up.")
-        else:
-            self.mat[self.blank] = self.mat[self.blank[0] + 1, self.blank[1]]
-            self.blank = (self.blank[0] + 1, self.blank[1])
-            self.mat[self.blank] = BLANK_KEY
+def up(self2: Matrix):
+    self = deepcopy(self2)
+    if self.blank[0] == ROW_SIZE - 1:
+        print("No tile can be moved up.")
+    else:
+        self.mat[self.blank] = self.mat[self.blank[0] + 1, self.blank[1]]
+        self.blank = (self.blank[0] + 1, self.blank[1])
+        self.mat[self.blank] = BLANK_KEY
+        return self
 
-    def down(self):
-        if self.blank[0] == 0:
-            print("No tile can be moved down.")
-        else:
-            self.mat[self.blank] = self.mat[self.blank[0] - 1, self.blank[1]]
-            self.blank = (self.blank[0] - 1, self.blank[1])
-            self.mat[self.blank] = BLANK_KEY
+def down(self2: Matrix):
+    self = deepcopy(self2)
+    if self.blank[0] == 0:
+        print("No tile can be moved down.")
+    else:
+        self.mat[self.blank] = self.mat[self.blank[0] - 1, self.blank[1]]
+        self.blank = (self.blank[0] - 1, self.blank[1])
+        self.mat[self.blank] = BLANK_KEY
+        return self
 
-    def left(self):
-        if self.blank[1] == COL_SIZE - 1:
-            print("No tile can be moved left.")
-        else:
-            self.mat[self.blank] = self.mat[self.blank[0], self.blank[1] + 1]
-            self.blank = (self.blank[0], self.blank[1] + 1)
-            self.mat[self.blank] = BLANK_KEY
+def left(self2: Matrix):
+    self = deepcopy(self2)
+    if self.blank[1] == COL_SIZE - 1:
+        print("No tile can be moved left.")
+    else:
+        self.mat[self.blank] = self.mat[self.blank[0], self.blank[1] + 1]
+        self.blank = (self.blank[0], self.blank[1] + 1)
+        self.mat[self.blank] = BLANK_KEY
+        return self
 
-    def right(self):
-        if self.blank[1] == 0:
-            print("No tile can be moved left.")
-        else:
-            self.mat[self.blank] = self.mat[self.blank[0], self.blank[1] - 1]
-            self.blank = (self.blank[0], self.blank[1] - 1)
-            self.mat[self.blank] = BLANK_KEY
+def right(self2: Matrix):
+    self = deepcopy(self2)
+    if self.blank[1] == 0:
+        print("No tile can be moved left.")
+    else:
+        self.mat[self.blank] = self.mat[self.blank[0], self.blank[1] - 1]
+        self.blank = (self.blank[0], self.blank[1] - 1)
+        self.mat[self.blank] = BLANK_KEY
+        return self
 
 
 mat = Matrix()
-mat.up()
-mat.right()
-mat.down()
-print(mat.mat)
+print(right(mat).mat)
+print(down(mat).mat)
