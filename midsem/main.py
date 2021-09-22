8c53c273435a59718597676bdb1c4590817c0d38
Abhishek Chopra <abhishekchopra13@hotmail.com>
2021-09-23 03:42:54 +0530

    Final Commit

--- midsem/main.py
+++ midsem/main.py
@@ -13,21 +13,21 @@ def main():
     print("Printing details for Vocaulary:\n")
     print("Word: (times in DL, times in CV)")
     # get prediction, parameters and score for corresponding model
-    results_multinomial, pxc, score = multinomial_naive_bayes(train_data, test)
-    results_multivariate, pxc_var, score_var = multivariate_naive_bayes(train_data, test)
+    results_multinomial, pxc_multinomial, score_multinomial = multinomial_naive_bayes(train_data, test)
+    results_multivariate, pxc_multivariate, score_multivariate = multivariate_naive_bayes(train_data, test)
     print("")
-    print(f"(i) Prediction of Multivariate Naive Bayes: {results_multivariate} with confidence: {score}")
-    print(f"(ii) Prediction of Multinomial Naive Bayes: {results_multinomial} with confidence: {score_var}")
+    print(f"(i) Prediction of Multivariate Naive Bayes: {results_multivariate} with confidence: {score_multivariate}")
+    print(f"(ii) Prediction of Multinomial Naive Bayes: {results_multinomial} with confidence: {score_multinomial}")
     print("")
     print("Value of parameters for Multinomial:")
     print("Word: (parameter for DL, for CV)\n")
-    for key in pxc:
-        print(f"{key}: {pxc[key]}")
+    for key in pxc_multinomial:
+        print(f"{key}: {pxc_multinomial[key]}")
     print("")
     print("Value of parameters for Multivariate:")
     print("Word: (parameter for DL, for CV)\n")
-    for key in pxc_var:
-        print(f"{key}: {pxc_var[key]}")
+    for key in pxc_multivariate:
+        print(f"{key}: {pxc_multivariate[key]}")
 
 if __name__ == "__main__":
     main()
