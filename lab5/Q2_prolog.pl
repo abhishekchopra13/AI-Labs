7c95a3d59d4fa033512a1ca71a4189d6a9b703cb
Shashwat Mahajan <shashwat211@gmail.com>
2021-10-27 11:09:28 +0530

    Update order

--- lab5/Q2_prolog.pl
+++ lab5/Q2_prolog.pl
@@ -6,10 +6,6 @@ himalayan_club(c).
 himalayan_club(X):-notmc(X),notsk(X),!, fail.
 himalayan_club(_).
 
-% If P likes Q, then fail
-dislike(P,Q):- like(P,Q),!,fail.
-dislike(_,__).
-
 like(a,rain).
 like(a,snow).
 
@@ -21,7 +17,12 @@ mc(X):-like(X,rain),!,fail.
 mc(_).
 
 notsk(X):- dislike(X,snow).
+
 notmc(X):- mc(X),!,fail.
 notmc(_).
+
+% If P likes Q, then fail
+dislike(P,Q):- like(P,Q),!,fail.
+dislike(_,__).
 
 query(X):-himalayan_club(X),mc(X),notsk(X),!.
