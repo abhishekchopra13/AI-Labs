6f26784ce2143e8ec1d8ff4dd74c9d210aeeb63c
Shashwat Mahajan <shashwat211@gmail.com>
2021-10-27 09:39:40 +0530

    Update prolog file
    - Added corrections

--- lab5/Q2_prolog.pl
+++ lab5/Q2_prolog.pl
@@ -2,15 +2,24 @@ himalayan_club(a).
 himalayan_club(b).
 himalayan_club(c).
 
-himalayan_club(A):-mountain_climber(A);skier(A).
+himalayan_club(X):-notmc(X),notsk(X),!, fail.
+himalayan_club(_).
 
-like(a, snow).
-like(a, rain).
+dislike(P,Q):- like(P,Q),!,fail.
+dislike(_,__).
 
-dislike(b, A):-like(a, A).
-dislike(a, A):-like(b, A).
+like(a,rain).
+like(a,snow).
 
-skier(A):-like(A, snow).
-mountain_climber(A):-dislike(A, rain).
+like(a,X) :- dislike(b,X).
+like(b,X) :- like(a,X),!,fail.
+like(b,_).
 
-query(D):-himalayan_club(D), mountain_climber(D), not(skier(D)),!.
+mc(X):-like(X,rain),!,fail.
+mc(_).
+
+notsk(X):- dislike(X,snow).
+notmc(X):- mc(X),!,fail.
+notmc(_).
+
+query(X):-himalayan_club(X),mc(X),notsk(X),!.
